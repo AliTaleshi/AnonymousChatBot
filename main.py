c0fcb26bc766a7b0667848617e3de4d65f5451b0
Alireza Khodadoust <khodadoust@jibit.ir>
2023-10-13 20:21:34 +0330

    Use Enviromental Varible for Bot Token

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import os
 from typing import Final
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
-Token: Final = ''
+TOKEN: Final = os.environ.get('ANONYMOUS_CHAT_BOT_TOKEN')
 BOT_USERNAME: Final = '@AnonymousChat0_bot'
 
 
