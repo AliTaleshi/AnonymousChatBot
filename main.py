67326618eab92af5c6954758c48c1927cd10fe4f
Alireza Khodadoust <khodadoust@jibit.ir>
2023-10-13 21:19:15 +0330

    Start Bot Successfully

--- main.py
+++ main.py
@@ -7,5 +7,24 @@ TOKEN: Final = os.environ.get('ANONYMOUS_CHAT_BOT_TOKEN')
 BOT_USERNAME: Final = '@AnonymousChat0_bot'
 
 
+# handlers
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello World!")
+    await update.message.reply_text("به ربات چت ناشناس خوش آمدید! 😀")
+
+
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f'Update {update} caused error {context.error}')
+
+
+if __name__ == '__main__':
+    app = Application.builder().token(TOKEN).build()
+
+    # Commands
+    app.add_handler(CommandHandler('start', start_command))
+
+    # Log all errors
+    app.add_error_handler(error)
+
+    print('Polling...')
+    # Run the bot
+    app.run_polling(poll_interval=3)
